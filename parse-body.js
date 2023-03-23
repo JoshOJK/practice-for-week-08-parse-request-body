ddcd2d76e1d58aa80473d2d1e75ab1bd97d72374
joshjk <jokrienke@comcast.net>
2023-03-22 21:55:21 -0700

    all-tests-passed

--- parse-body.js
+++ parse-body.js
@@ -1,25 +1,39 @@
 function firstStep(input) {
-  // Your code here
+  return input.split('&')
 }
 
 function secondStep(input) {
-  // Your code here
+  return input.map((string) => string.split('='))
 }
 
 function thirdStep(input) {
-  // Your code here
+  input.map((pair) => pair[1] = pair[1].replace(/\+/g, " "))
+  return input;
 }
 
 function fourthStep(input) {
-  // Your code here
+input.map((pair) => pair[1] = decodeURIComponent(pair[1]))
+return input
 }
 
 function fifthStep(input) {
-  // Your code here
+  const obj = {}
+  for (let i = 0; i < input.length; i++) {
+    let keyVal = input[i]
+    let key = input[i][0]
+    let val = input[i][1]
+    obj[key] = val;
+  }
+  return obj
 }
 
 function parseBody(str) {
-  // Your code here
+  let input = firstStep(str)
+  let input1 = secondStep(input)
+  let input2 = thirdStep(input1)
+  let input3 = fourthStep(input2)
+  let input4 = fifthStep(input3)
+  return input4;
 }
 
 /******************************************************************************/
--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const { sendFormPage } = require("./routes");
 const { parseBody } = require("./parse-body");
-let server;
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
 
-// Your code here
+const http = require('http')
+
+let server = http.createServer((req, res) => {
+    let reqBody = "";
+    req.on('data', (data) => {
+        reqBody += data
+    })
+ if (req.method === 'GET' && req.url === '/') {
+    let method = req.method;
+    let url = req.url;
+    console.log( method, url);
+  }
+    req.on('end', () => {
+        if(reqBody) req.body = parseBody(reqBody)
+        sendFormPage(req, res)
+    })
+})
+
+const port = 5000;
+
+server.listen(port, () => console.log(`Successfully started the server on port ${port}`))
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
